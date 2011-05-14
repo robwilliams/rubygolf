153bc15e6be36649be206fc166fbe679152cae8a
Robert Williams <rob@r-williams.com>
2011-05-14 13:01:46 +0100

    only 17 tests to go

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,25 @@
 class Golf
+
+  class << self
+    
+    def hole1(a)
+      r = 1
+      a.each {|x| r *= x}
+      r
+    end
+    
+    def hole2(s)
+      s.split(' ').sort {|x, y| x[1] <=> y[1]} * ' '
+    end
+    
+    def hole3(i)
+      r = 1
+      i.downto(1) {|d| r *= d}
+      r
+    end
+    
+    def hole4(a)
+      a.collect {|x| x.gsub("cat", "dead").gsub(/(man\(.*\))/, "hat(\\1)").gsub(/(dog\(.*)\)/, "\\1(bone))")}
+    end
+  end
 end
