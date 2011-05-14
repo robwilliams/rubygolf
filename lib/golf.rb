7c0832f3396f92d4701f2e7b488442565c9ad45c
Robert Williams <rob@r-williams.com>
2011-05-14 13:41:26 +0100

    this isn't going to work for the second spec

--- lib/golf.rb
+++ lib/golf.rb
@@ -21,5 +21,15 @@ class Golf
     def hole4(a)
       a.collect {|x| x.gsub("cat", "dead").gsub(/(man\(.*\))/, "hat(\\1)").gsub(/(dog\(.*)\)/, "\\1(bone))")}
     end
+    
+    def hole5(a)
+      r = []
+      a.length.times {|c|
+        r += a.permutation(c+1).to_a.collect{|x| x.sort}.reject{|x| 
+          x.length < 2 ? false : x[1] != x[0].succ || x[-1] != x[-2].succ
+        }.uniq
+      }
+      r
+    end
   end
 end
